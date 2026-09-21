9d09514cfa67ca664c34f3f83f6bfdc595f8980a
pehab <65153752+pehab@users.noreply.github.com>
2026-09-21 15:30:00 +0200

    Fix generated R and BuildConfig imports

--- app/src/main/java/de/haberland/meiocrworkout/ui/screens/ProfilesScreen.kt
+++ app/src/main/java/de/haberland/meiocrworkout/ui/screens/ProfilesScreen.kt
@@ -1,5 +1,7 @@
 package de.haberland.meiocrworkout.ui.screens
 
+import de.haberland.meiocrworkout.R
+import de.haberland.meiocrworkout.BuildConfig
 import de.haberland.meiocrworkout.ui.*
 import de.haberland.meiocrworkout.ui.screens.*
 import de.haberland.meiocrworkout.presentation.*
