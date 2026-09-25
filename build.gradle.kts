0d4834ffe82f0fc8a14d16279a6e93daed06ef3c
pehab <65153752+pehab@users.noreply.github.com>
2026-09-25 10:22:26 +0200

    Firebase-Plugins registrieren

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
     id("com.google.devtools.ksp") version "2.3.10" apply false
     id("androidx.room") version "2.8.5" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.firebase.crashlytics") version "3.0.7" apply false
 }
